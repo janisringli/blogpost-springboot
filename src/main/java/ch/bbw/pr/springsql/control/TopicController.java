50b30fb5b5b1446201873b9b6313728349e44519
Janis Ringli <janis.ringli@gmail.com>
2023-01-12 16:37:55 +0100

    add tables and controllers to topic

--- src/main/java/ch/bbw/pr/springsql/control/TopicController.java
+++ src/main/java/ch/bbw/pr/springsql/control/TopicController.java
@@ -1,4 +1,16 @@
 package ch.bbw.pr.springsql.control;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class TopicController {
+    @GetMapping(path="/getalltopics")
+    public String getalltopics() {
+        return "getalltopics";
+    }
+    @GetMapping(path="/gettopicbyid")
+    public String gettopicbyid() {
+        return "gettopicbyid";
+    }
 }
--- src/main/java/ch/bbw/pr/springsql/model/Topic.java
+++ src/main/java/ch/bbw/pr/springsql/model/Topic.java
@@ -1,4 +1,35 @@
 package ch.bbw.pr.springsql.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
 public class Topic {
+    @Id
+    private int id;
+    private String topicName;
+
+    public Topic() {
+    }
+
+    public Topic(int id, String topicName) {
+        this.id = id;
+        this.topicName = topicName;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return topicName;
+    }
+
+    public void setName(String topicName) {
+        this.topicName = topicName;
+    }
 }
