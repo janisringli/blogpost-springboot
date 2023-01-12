1a670bfb4bbb3c9556c2282cccc31c55e40b5373
Janis Ringli <janis.ringli@gmail.com>
2023-01-12 16:27:34 +0100

    add tables and controllers to author

--- src/main/java/ch/bbw/pr/springsql/control/AuthorController.java
+++ src/main/java/ch/bbw/pr/springsql/control/AuthorController.java
@@ -1,4 +1,19 @@
 package ch.bbw.pr.springsql.control;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class AuthorController {
+    @GetMapping(path="/getallauthors")
+    public String getallauthors() {
+        return "getallauthors";
+    }
+    @GetMapping(path="/getauthorbyid")
+    public String getauthorbyid() {
+        return "getauthorbyid";
+    }
+
+
 }
--- src/main/java/ch/bbw/pr/springsql/model/Author.java
+++ src/main/java/ch/bbw/pr/springsql/model/Author.java
@@ -1,4 +1,65 @@
 package ch.bbw.pr.springsql.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
 public class Author {
+    @Id
+    private int id;
+    private String name;
+    private String email;
+    private String password;
+    private String bio;
+
+    public Author() {
+    }
+
+    public Author(int id, String name, String email, String password, String role) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.bio = bio;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return bio;
+    }
+
+    public void setRole(String role) {
+        this.bio = role;
+    }
 }
