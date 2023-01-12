edfd3f9a1cc405ca237df7162603d7298a18fde2
Janis Ringli <janis.ringli@gmail.com>
2023-01-12 16:36:01 +0100

    add tables and controllers to saved article

--- src/main/java/ch/bbw/pr/springsql/control/SavedArticlesController.java
+++ src/main/java/ch/bbw/pr/springsql/control/SavedArticlesController.java
@@ -1,4 +1,17 @@
 package ch.bbw.pr.springsql.control;
 
+import org.springframework.web.bind.annotation.RestController;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+@RestController
 public class SavedArticlesController {
+    @GetMapping(path="/getallsavedarticles")
+    public String getallsavedarticles() {
+        return "getallsavedarticles";
+    }
+    @GetMapping(path="/getsavedarticlebyid")
+    public String getsavedarticlebyid() {
+        return "getsavedarticlebyid";
+    }
 }
--- src/main/java/ch/bbw/pr/springsql/model/SavedArticles.java
+++ src/main/java/ch/bbw/pr/springsql/model/SavedArticles.java
@@ -1,5 +1,46 @@
 package ch.bbw.pr.springsql.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
 public class SavedArticles {
+    @Id
+    private int id;
+    private int articleId;
+    private int authorId;
+
+    public SavedArticles() {
+    }
+
+    public SavedArticles(int id, int articleId, int authorId) {
+        this.id = id;
+        this.articleId = articleId;
+        this.authorId = authorId;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getArticleId() {
+        return articleId;
+    }
+
+    public void setArticleId(int articleId) {
+        this.articleId = articleId;
+    }
+
+    public int getAuthorId() {
+        return authorId;
+    }
+
+    public void setAuthorId(int authorId) {
+        this.authorId = authorId;
+    }
 
 }
