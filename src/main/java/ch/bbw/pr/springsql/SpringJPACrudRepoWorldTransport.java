e8a43a1546769df1c57db66a9077ef878f676304
Janis Ringli <janis.ringli@gmail.com>
2023-01-12 16:08:51 +0100

    init

---
+++ src/main/java/ch/bbw/pr/springsql/SpringJPACrudRepoWorldTransport.java
@@ -0,0 +1,13 @@
+package ch.bbw.pr.springsql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJPACrudRepoWorldTransport {
+
+   public static void main(String[] args) {
+      SpringApplication.run(SpringJPACrudRepoWorldTransport.class, args);
+   }
+
+}
---
+++ src/test/java/ch/bbw/pr/springsql/SpringJPACrudRepoWorldTransportTest.java
@@ -0,0 +1,13 @@
+package ch.bbw.pr.springsql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJPACrudRepoWorldTransportTest {
+
+   @Test
+   void contextLoads() {
+   }
+
+}
