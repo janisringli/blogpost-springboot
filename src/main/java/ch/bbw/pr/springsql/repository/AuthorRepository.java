82da35d63a7bd2c89b5e89c8535f9c1d7661e336
Janis Ringli <janis.ringli@gmail.com>
2023-01-24 20:08:44 +0100

    add function for author repository

--- src/main/java/ch/bbw/pr/springsql/repository/AuthorRepository.java
+++ src/main/java/ch/bbw/pr/springsql/repository/AuthorRepository.java
@@ -1,2 +1,7 @@
-package ch.bbw.pr.springsql.repository;public interface AuthorRepository {
+package ch.bbw.pr.springsql.repository;
+
+import ch.bbw.pr.springsql.model.Author;
+import org.springframework.data.repository.CrudRepository;
+
+public interface AuthorRepository extends CrudRepository<Author, Integer> {
 }
