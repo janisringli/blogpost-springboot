bcf57d67115cf994eb3590c1975135ce089100db
Janis Ringli <janis.ringli@gmail.com>
2023-01-24 20:09:49 +0100

    add function for savedarticles repository

--- src/main/java/ch/bbw/pr/springsql/repository/SavedArticlesRepository.java
+++ src/main/java/ch/bbw/pr/springsql/repository/SavedArticlesRepository.java
@@ -1,2 +1,7 @@
-package ch.bbw.pr.springsql.repository;public interface SavedArticlesRepository {
+package ch.bbw.pr.springsql.repository;
+
+import ch.bbw.pr.springsql.model.SavedArticles;
+import org.springframework.data.repository.CrudRepository;
+
+public interface SavedArticlesRepository extends CrudRepository<SavedArticles, Integer> {
 }
