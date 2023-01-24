76160b22d09a1fd953c2baff30a6e26ca1bf62bf
Janis Ringli <janis.ringli@gmail.com>
2023-01-24 20:47:51 +0100

    add all crud operations to savedArticle

--- src/main/java/ch/bbw/pr/springsql/controller/SavedArticlesController.java
+++ src/main/java/ch/bbw/pr/springsql/controller/SavedArticlesController.java
@@ -1,17 +1,39 @@
-package ch.bbw.pr.springsql.control;
+package ch.bbw.pr.springsql.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import ch.bbw.pr.springsql.model.SavedArticles;
+import ch.bbw.pr.springsql.repository.SavedArticlesRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
-
-import org.springframework.web.bind.annotation.GetMapping;
 @RestController
 public class SavedArticlesController {
-    @GetMapping(path="/getallsavedarticles")
-    public String getallsavedarticles() {
-        return "getallsavedarticles";
+    @Autowired
+    private SavedArticlesRepository savedArticlesRepo;
+    @GetMapping(path="/savedarticles")
+    public Iterable<SavedArticles> getSavedArticles() {
+        return savedArticlesRepo.findAll();
     }
-    @GetMapping(path="/getsavedarticlebyid")
-    public String getsavedarticlebyid() {
-        return "getsavedarticlebyid";
+    @GetMapping(path="/savedarticle/{id}")
+    public SavedArticles savedArticlesById(@PathVariable("id")int id) {
+        return savedArticlesRepo.findById(id).get();
     }
+    @PostMapping(path="/savedarticles")
+    public SavedArticles addSavedArticles(@RequestBody SavedArticles savedArticles) {
+        return savedArticlesRepo.save(savedArticles);
+    }
+    @PutMapping(path="/savedarticle/{id}")
+    public SavedArticles updateSavedArticles(@PathVariable("id")int id, @RequestBody SavedArticles savedArticles) {
+        SavedArticles savedArticlesToUpdate = savedArticlesRepo.findById(id).get();
+        savedArticlesToUpdate.setArticleId(savedArticles.getArticleId());
+        savedArticlesToUpdate.setAuthorId(savedArticles.getAuthorId());
+        return savedArticlesRepo.save(savedArticlesToUpdate);
+    }
+    @DeleteMapping(path="/savedarticle/{id}")
+    public void deleteSavedArticles(@PathVariable("id")int id) {
+        savedArticlesRepo.deleteById(id);
+    }
+
+
+
+
 }
