90c84f1d97ee5a9025aafed8ee6f05f08fa6ebcf
Janis Ringli <janis.ringli@gmail.com>
2023-01-25 16:49:35 +0100

    add saved article as list to repository

--- src/main/java/ch/bbw/pr/springsql/repository/SavedArticlesRepository.java
+++ src/main/java/ch/bbw/pr/springsql/repository/SavedArticlesRepository.java
@@ -1,7 +1,10 @@
 package ch.bbw.pr.springsql.repository;
 
+import ch.bbw.pr.springsql.model.Author;
 import ch.bbw.pr.springsql.model.SavedArticles;
 import org.springframework.data.repository.CrudRepository;
 
 public interface SavedArticlesRepository extends CrudRepository<SavedArticles, Integer> {
+    Iterable<SavedArticles> findByAuthorId(int id);
+
 }
